0799adac83997feb5858ee9dced1c0ef818cc2b3
Luis <luis.huescag@outlook.com>
2026-02-02 15:19:00 -0600

    Añado los tipos de texto en base a la actividad 1

---
+++ Introduccion a javascript/tipo-de-datos.js
@@ -0,0 +1,21 @@
+// Tipos datos
+
+// Strings
+console.log (typeof("Hola soy un string"));
+console.log(typeof("Veinticinco"));
+console.log(typeof(""));
+
+// Numbers
+console.log(typeof(42));
+console.log(typeof(-666));
+console.log(typeof(0));
+
+
+// Booleanos
+console.log(typeof(true));
+console.log(typeof(false));
+
+// Nulos e indefinidos
+console.log(typeof(null));
+console.log(typeof(undefined));
+
