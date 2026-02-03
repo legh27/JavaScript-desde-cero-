534d8e6ec52fc41dd1e706cfcd55312b1a81e8fd
Luis <luis.huescag@outlook.com>
2026-02-02 19:20:32 -0600

    Aqui agrego el archivo de la 2da actividad.

---
+++ Comparaciones y decisiones en javascript/Comparaciones y decisiones.js
@@ -0,0 +1,14 @@
+let nota = ;
+
+if (nota >= 90) {
+    console.log("Excelente");
+}
+else if (nota >= 75 && nota <= 89) {
+    console.log("Bien");
+}
+else if (nota >= 60 && nota <= 74) {
+    console.log("Suficiente");
+}
+else {
+    console.log("Reprobado");
+}
